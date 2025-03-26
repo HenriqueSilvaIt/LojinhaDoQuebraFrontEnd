054cee7c66e8d4156bc6a1fba5d68fa6af8b8bc2
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-03-25 23:11:34 -0300

    update url

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -4,7 +4,7 @@ export const TOKEN_KEy = "com.devsuperior.ecommerce/art";
 
 /* coloca variavel de ambiente e caso n esteja configura ??(operador de coalesencia nula, se n existir
  variavel da esquerda pega por padrão o valo da direita) colcoa um valor padrão no lugar */
- export const BASE_URL = import.meta.env.BACKEND_URL ?? "https://lojinhadoquebrabackend-production.up.railway.app"  /* se 
+ export const BASE_URL = import.meta.env.BACKEND_URL ?? "https://lojinhadoquebrabackend-production.up.railway.app";  /* se 
 // tiver configurado na ferramenta na nuvem a variavel, por padrão tem que ser pega
 // diretóreto de lá,  mas com oestamos rodando na máquina vamos coloc aqu 
 // */
